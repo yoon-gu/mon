4201bcdd96754b3f1ea4608fd0a363a8e6b4024e
Yoon-gu Hwang <yz0624@gmail.com>
2023-10-19 15:23:25 +0900

    backup

--- make_problems.py
+++ make_problems.py
@@ -2,8 +2,8 @@ import json
 import os
 import random
 
-TEMPLATE = "\item ~ \\newline \opadd[resultstyle=\placeholder,carrystyle=\color{{white}},displayintermediary=None]{{{input1}}}{{{input2}}}"
-MULT_TEMPLATE = "\item ~ \\newline \opmul[resultstyle=\placeholder,carrystyle=\color{{white}},displayintermediary=None]{{{input1}}}{{{input2}}}"
+TEMPLATE = "\item ~\\newline \opadd[resultstyle=\placeholder,carrystyle=\color{{white}},displayintermediary=None,voperation=top,voperator=bottom]{{{input1}}}{{{input2}}}"
+MULT_TEMPLATE = "\item ~\\newline \opmul[resultstyle=\placeholder,carrystyle=\color{{white}},displayintermediary=None,voperation=top,voperator=bottom]{{{input1}}}{{{input2}}}"
 def add_problem(lower, upper):
     input1 = random.randint(lower, upper)
     input2 = random.randint(lower, upper)
@@ -17,19 +17,28 @@ def multiply_problem(lower, upper):
     return {"input1": input1, "input2": input2, "solution": solution}
 
 problems = []
+# num_problems = 30
+# for _ in range(num_problems):
+#     problems.append(MULT_TEMPLATE.format_map(multiply_problem(1, 20)))
+num_problems = 30
+for _ in range(num_problems):
+    problems.append(TEMPLATE.format_map(add_problem(1, 20)))
+
+with open('add_problem.tex', 'w') as f:
+    f.write('\n'.join(problems))
+
+problems = []
 num_problems = 30
 for _ in range(num_problems):
     problems.append(MULT_TEMPLATE.format_map(multiply_problem(1, 20)))
 
-print('\n'.join(problems))
-
-with open('add_problem.tex', 'w') as f:
+with open('multiply_problem.tex', 'w') as f:
     f.write('\n'.join(problems))
 
 os.system('pdflatex example.tex')
 import time
 time.sleep(1)
-os.system('cp example.pdf add_problem.pdf')
+os.system('cp example.pdf problem.pdf')
 
 print('\n'.join(problems))
 
@@ -38,4 +47,4 @@ with open('add_problem.tex', 'w') as f:
 
 os.system('pdflatex example.tex')
 time.sleep(1)
-os.system('cp example.pdf add_problem_solution.pdf')
+os.system('cp example.pdf solution.pdf')
