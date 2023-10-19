6cd1485b8f744881dfe8f2c7a2b8e9ac2d9fe165
Yoon-gu Hwang <yz0624@gmail.com>
2023-10-19 14:04:29 +0900

    backup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# mon
+# 학습지 만들기
+
+## Useful links
+
+1. https://tex.stackexchange.com/questions/64946/hide-solution-when-using-xlop
+1. https://tex.stackexchange.com/questions/672540/pythontex-and-xlop
---
+++ make_problems.py
@@ -0,0 +1,41 @@
+import json
+import os
+import random
+
+TEMPLATE = "\item ~ \\newline \opadd[resultstyle=\placeholder,carrystyle=\color{{white}},displayintermediary=None]{{{input1}}}{{{input2}}}"
+MULT_TEMPLATE = "\item ~ \\newline \opmul[resultstyle=\placeholder,carrystyle=\color{{white}},displayintermediary=None]{{{input1}}}{{{input2}}}"
+def add_problem(lower, upper):
+    input1 = random.randint(lower, upper)
+    input2 = random.randint(lower, upper)
+    solution = input1 + input2
+    return {"input1": input1, "input2": input2, "solution": solution}
+
+def multiply_problem(lower, upper):
+    input1 = random.randint(lower, upper)
+    input2 = random.randint(lower, upper)
+    solution = input1 + input2
+    return {"input1": input1, "input2": input2, "solution": solution}
+
+problems = []
+num_problems = 30
+for _ in range(num_problems):
+    problems.append(MULT_TEMPLATE.format_map(multiply_problem(1, 20)))
+
+print('\n'.join(problems))
+
+with open('add_problem.tex', 'w') as f:
+    f.write('\n'.join(problems))
+
+os.system('pdflatex example.tex')
+import time
+time.sleep(1)
+os.system('cp example.pdf add_problem.pdf')
+
+print('\n'.join(problems))
+
+with open('add_problem.tex', 'w') as f:
+    f.write('\n'.join([p.replace("resultstyle=\placeholder,", "") for p in problems]))
+
+os.system('pdflatex example.tex')
+time.sleep(1)
+os.system('cp example.pdf add_problem_solution.pdf')
