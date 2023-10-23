3a801a65c3dd8bc2a466285ff18416c0ab56e9ba
Yoon-gu Hwang <yz0624@gmail.com>
2023-10-23 21:31:50 +0900

    우주

--- make_problems.py
+++ make_problems.py
@@ -3,7 +3,7 @@ import os
 import random
 
 TEMPLATE = "\item  \opadd[resultstyle=\placeholder,carryadd=false,displayintermediary=None,voperation=top,voperator=bottom]{{{input1}}}{{{input2}}}"
-MULT_TEMPLATE = "\item  \opmul[resultstyle=\placeholder,carryadd=false,displayintermediary=None,voperation=top,voperator=bottom]{{{input1}}}{{{input2}}}"
+MULT_TEMPLATE = "\item  \opmul[resultstyle=\placeholder,carryadd=false,voperation=top,voperator=bottom]{{{input1}}}{{{input2}}}"
 def add_problem(lower, upper):
     input1 = random.randint(lower, upper)
     input2 = random.randint(lower, upper)
@@ -19,7 +19,7 @@ def multiply_problem(lower, upper):
 add_problems = []
 num_problems = 30
 for _ in range(num_problems):
-    add_problems.append(TEMPLATE.format_map(add_problem(1, 20)))
+    add_problems.append(TEMPLATE.format_map(add_problem(1000, 9999)))
 
 with open('add_problem.tex', 'w') as f:
     f.write('\n'.join(add_problems))
@@ -27,7 +27,7 @@ with open('add_problem.tex', 'w') as f:
 mul_problems = []
 num_problems = 30
 for _ in range(num_problems):
-    mul_problems.append(MULT_TEMPLATE.format_map(multiply_problem(1, 9)))
+    mul_problems.append(MULT_TEMPLATE.format_map(multiply_problem(1000, 7777)))
 
 with open('multiply_problem.tex', 'w') as f:
     f.write('\n'.join(mul_problems))
@@ -38,10 +38,10 @@ time.sleep(1)
 os.system('cp example.pdf problem.pdf')
 
 with open('add_problem.tex', 'w') as f:
-    f.write('\n'.join([p.replace("resultstyle=\placeholder,", "") for p in add_problems]))
+    f.write('\n'.join([p.replace("resultstyle=\placeholder,", "").replace("carryadd=false","carryadd=true") for p in add_problems]))
 
 with open('multiply_problem.tex', 'w') as f:
-    f.write('\n'.join([p.replace("resultstyle=\placeholder,", "") for p in mul_problems]))
+    f.write('\n'.join([p.replace("resultstyle=\placeholder,", "").replace("carryadd=false","carryadd=true") for p in mul_problems]))
 
 os.system('pdflatex example.tex')
 time.sleep(1)
